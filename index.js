7f75e538c4784e5524da51a4f8d7e8358294b0ca
pkc232 <pratyushc232@gmail.com>
2021-01-09 17:32:24 +0530

    Setting up the database config.

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json()); //req.body
 
-app.use(express.static(path.join(__dirname, "client/build")));
+// app.use(express.static(path.join(__dirname, "client/build")));
 
 if(process.env.NODE_ENV === "production"){
     //serve static content
