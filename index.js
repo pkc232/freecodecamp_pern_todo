a200a5d2c5d7f413ed4ea8df7d264ebf8870ba52
pkc232 <pratyushc232@gmail.com>
2021-01-09 17:53:26 +0530

    Set the engine version and catch random URLs.

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json()); //req.body
 
-// app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "client/build")));
 
 if(process.env.NODE_ENV === "production"){
     //serve static content
@@ -94,6 +94,9 @@ app.delete("/todos/:id", async(req, res) => {
     }
 });
 
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build/index.html"));
+})
 
 app.listen(PORT, () => {
     console.log(`Server has started on port ${PORT}`);
