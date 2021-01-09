b97e82b1e649c577a5c1671c9a26fccd1658d50d
pkc232 <pratyushc232@gmail.com>
2021-01-09 19:37:47 +0530

    Changing SSL again.

--- db.js
+++ db.js
@@ -8,12 +8,11 @@ const devConfig = {
     host: process.env.PG_HOST,
     port: process.env.PG_PORT,
     database: process.env.PG_DATABASE,
-    ssl: true
 };
 
 const proConfig = {
     connectionString: process.env.DATABASE_URL, //heroku addon
-    ssl: true
+    ssl: { rejectUnauthorized: false }
 };
 
 const pool = new Pool(process.env.NODE_ENV === "production" ? proConfig : devConfig);
