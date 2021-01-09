6cbaf0ac664dc37123c0808022f18d03ebcc1343
pkc232 <pratyushc232@gmail.com>
2021-01-09 17:19:22 +0530

    Section 1. Doing the build.

--- index.js
+++ index.js
@@ -2,10 +2,24 @@ const express = require('express');
 const app = express();
 const cors = require("cors");
 const pool = require("./db");
+const path = require("path");
+
+const PORT = process.env.PORT || 5000;
+
+//process.env.PORT
+//process.env.NODE_ENV => production or undefined
 
 //middleware
 app.use(cors());
 app.use(express.json()); //req.body
+
+app.use(express.static(path.join(__dirname, "client/build")));
+
+if(process.env.NODE_ENV === "production"){
+    //serve static content
+    //npm run build
+    app.use(express.static(path.join(__dirname, "client/build")));
+}
 
 //ROUTES//
 
@@ -81,6 +95,6 @@ app.delete("/todos/:id", async(req, res) => {
 });
 
 
-app.listen(5000, () => {
-    console.log("Server has started on port 5000");
+app.listen(PORT, () => {
+    console.log(`Server has started on port ${PORT}`);
 })
